f5aad880ecea2616a413a67ae263e52f83475b3a
bruce wene <brucewene73@gmail.com>
2025-02-13 19:08:00 +0900

    portfolio

---
+++ components/Footer.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import Link from "next/link";
+import { Button } from "./ui/MovingBorders";
+
+const Footer = () => {
+  return (
+    <footer className="w-full mb-[100px] pb-10 md:mb-5" id="contact">
+      <div className="flex flex-col items-center">
+        <h2 className="heading lg:max-w-[45vw]">
+          Looking forward
+          <span className="text-purple">
+            <div className="p-4 relative z-10 w-full text-center">
+              <Link href={"https://github.com/Frozen55Git"}>
+                <Button
+                  borderRadius="1.75rem"
+                  className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                >
+                  GitHub
+                </Button>
+              </Link>
+              <Link href={"https://www.linkedin.com/in/volodymyr-karabash-1460b0350/"}>
+                <Button
+                  borderRadius="1.75rem"
+                  className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                >
+                  LinkedIn
+                </Button>
+              </Link>
+            </div>
+          </span>{" "}
+          Contact Me
+        </h2>
+        <p className="text-white-200 md:mt-10 my-5 text-center"></p>
+        <a href="mailto:voca480@gmail.com">
+          <MagicButton
+            title="Let's get in Touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+
+      <div className="flex mt-16 md:flex-row flex-col justify-center items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Made with ♡ Volodymyr Karabash
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ data/index.ts
@@ -0,0 +1,142 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Skills", link: "#skills" },
+    { name: "Contact", link: "#contact" },
+   {name: "Download Resume",link:"https://drive.google.com/file/d/1s8509gV2bFKCfM2V0CrFlZjnnvV6vos1/view?usp=sharing"}
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize work collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently working on Next.Js Projects",
+      description: "",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Doctor appointment booking system using React.js and Node.js",
+      des: "This is a simple doctor appointment booking system using React.js and Node.js. The frontend is built using React.js and the backend is built using Node.js. The backend is a simple REST API that serves the frontend. The frontend is a single page application that allows users to book appointments with doctors. The appointments are stored in a MongoDB database.",
+      img: "/doctor.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/Frozen55Git/Hospitality-Business",
+    },
+    {
+      id: 2,
+      title: "Shop",
+      des: "Developed and implemented essential e-commerce functionalities from scratch , including search, add-to-cart, checkout and remove-from-cart features with proper authentication.",
+      img: "/shop.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/Frozen55Git/Shop-Ecommerce",
+    },
+    {
+      id: 3,
+      title: "Online payment integration",
+      des: "This project showcases a PCI-compliant integration of the Sessions Flow, the default integration that we recommend for merchants. Explore this simplified e-commerce to discover the code, libraries and configuration you need to enable various payment options in your checkout experience.",
+      img: "/adyen-online.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/Frozen55Git/adyen-online-payments",
+    },
+    {
+      id: 4,
+      title: "Base Swap MarketPlace",
+      des: "Developed using the React, Nodejs, and web3 & Blockchain technologies , it offers a professional website with dynamic functionalities. Key features include Swap Exchange Function, Display Present Fund etc",
+      img: "/intro.jpg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/Frozen55Git/Base-Swap",
+    },
+  ];
+  
+
+ 
+
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Technologies",
+      desc: "Assisted in the development of a web-based platform using HTML,CSS,Javascipt,React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Backend Technologies",
+      desc: "Designed and developed using Node.js, Typescript, ExpressJs",
+      className: "md:col-span-2", 
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Full Stack Technologies",
+      desc: "Developement and designing using Next.js,Tailwind CSS,Bootsrap, Angular",
+      className: "md:col-span-2", 
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Database Management",
+      desc: "Developed and maintained user-facing features using MongoDB,SQL and Java for problem-solving approach.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+ 
